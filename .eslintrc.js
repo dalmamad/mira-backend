351c5f70dd6509a88d8d1a1dfbba36e2699e12cf
dalmamad <dal.mohamad97@gmail.com>
2023-03-11 22:27:42 +0330

    installing basic dev deps

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    // 'plugin:@typescript-eslint/recommended',
+    "airbnb-base",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // 'no-console': ['error', { allow: ['warn', 'error'] }],
+    "no-console": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
